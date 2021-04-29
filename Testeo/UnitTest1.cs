6134156ca572020ee3ac6fa684a1ae5a3e3e14b6
Oscar Poblete <os.pobleteq@alumnos.duoc.cl>
2021-04-29 13:58:24 -0400

    Agregar archivos de proyecto.

---
+++ Testeo/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testeo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
